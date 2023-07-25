16d3068b5ee534af7b80dd473dc73f6a7772c6fd
EllsaG <114601257+EllsaG@users.noreply.github.com>
2023-07-26 03:35:08 +0400

    STEP_8_JRTB-4: added ability to send notifications about new articles. (#24)
    STEP_8_JRTB-8: added ability to set inactive telegram user.
    STEP_8_JRTB-9: added ability to set active user and/or start using it.
    
    Co-authored-by: EllsaG <seninlsa@gmail.com>

---
+++ src/main/java/com/github/EllsaG/telegrambot/javarushclient/dto/LikesInfo.java
@@ -0,0 +1,9 @@
+package com.github.EllsaG.telegrambot.javarushclient.dto;
+/**
+ * DTO, which represents like's information.
+ */
+public class LikesInfo {
+
+    private Integer count;
+    private LikeStatus status;
+}
--- src/main/java/com/github/EllsaG/telegrambot/repository/entity/GroupSub.java
+++ src/main/java/com/github/EllsaG/telegrambot/repository/entity/GroupSub.java
@@ -29,7 +29,7 @@ public class GroupSub {
     private String title;
 
     @Column(name = "last_article_id")
-    private Integer lastArticleId;
+    private Integer lastPostId;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
